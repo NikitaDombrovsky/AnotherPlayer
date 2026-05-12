cecfe78d437da0cb57a164025349f461cfeddbbb
ATOM <128711042+Atom3311@users.noreply.github.com>
2026-05-12 18:08:32 +0600

    18:06

--- composeApp/src/commonMain/kotlin/org/example/project/ui/PlayerScreen.kt
+++ composeApp/src/commonMain/kotlin/org/example/project/ui/PlayerScreen.kt
@@ -1,13 +1,44 @@
 package org.example.project.ui
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.MusicNote
+import androidx.compose.material.icons.filled.Pause
+import androidx.compose.material.icons.filled.PlayArrow
+import androidx.compose.material.icons.filled.SkipNext
+import androidx.compose.material.icons.filled.SkipPrevious
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FilledIconButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.IconButtonDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Slider
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
+@Preview
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun PlayerScreen() {
@@ -20,13 +51,92 @@ fun PlayerScreen() {
                         onClick = {/*TODO*/}
                     ) {
                         Icon(
-
+                            imageVector = Icons.Filled.ArrowBack,
+                            contentDescription = "Back"
                         )
                     }
                 }
             )
         }
-    ) {
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+                .padding(horizontal = 32.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Box(
+                modifier = Modifier
+                    .size(300.dp)
+                    .clip(RoundedCornerShape(20.dp))
+                    .background(MaterialTheme.colorScheme.primaryContainer),
+                contentAlignment = Alignment.Center
+            ) {
+                Icon(
+                    imageVector = Icons.Filled.MusicNote,
+                    contentDescription = "",
+                    modifier = Modifier.size(100.dp),
+                    tint = MaterialTheme.colorScheme.onPrimaryContainer
+                )
+            }
 
+            Spacer(Modifier.height(20.dp))
+            Text(
+                text = "TODO",
+                style = MaterialTheme.typography.bodyLarge,
+            )
+            Spacer(Modifier.height(20.dp))
+
+            Slider(
+                value = 0.2f,
+                onValueChange = {/*TODO*/}
+            )
+
+            Spacer(Modifier.height(20.dp))
+
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceEvenly,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                IconButton(
+                    onClick = {/*TODO*/},
+                    modifier = Modifier.size(60.dp)
+                ) {
+                    Icon(
+                        imageVector = Icons.Filled.SkipPrevious,
+                        contentDescription = "Previous",
+                        modifier = Modifier.size(30.dp)
+                    )
+                }
+                val test = false
+                FilledIconButton(
+                    onClick = {/*TODO*/},
+                    modifier = Modifier.size(70.dp),
+                    shape = CircleShape,
+                    colors = IconButtonDefaults.filledIconButtonColors(
+                        containerColor = MaterialTheme.colorScheme.primary
+                    )
+                ){
+                    Icon(
+                        imageVector = if(test) Icons.Filled.Pause else Icons.Filled.PlayArrow,
+                        contentDescription = if (test) "Pause" else "Play",
+                        modifier = Modifier.size(40.dp)
+                    )
+                }
+                IconButton(
+                    onClick = {/*TODO*/},
+                    modifier = Modifier.size(60.dp)
+                ) {
+                    Icon(
+                        imageVector = Icons.Filled.SkipNext,
+                        contentDescription = "Next",
+                        modifier = Modifier.size(30.dp)
+                    )
+                }
+            }
+        }
     }
 }
